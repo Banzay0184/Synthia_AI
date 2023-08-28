c9c5ee4f827e2c34f0c9156123790197cf3e0f0d
Banzay0184 <shakhzodabidov@gmail.com>
2023-08-29 00:17:42 +0500

    0.1

---
+++ js/APIKey.js
@@ -0,0 +1,1 @@
+﻿var API_Key = decrypt("U2FsdGVkX187f+xwvAkucHME18oINVx2q6XQSvB/Si1G+vR3zamOiK5aREKKyg24bteE2gIk7yPQKdA7ypdo8iuHn+8G8hXJ4QHaJog3divB6aOMigKMpL+rnfSlIgqc", "4fcc7fd6-86ee-41ca-be6e-f3add9c35af9"); //Replace with key generated at http://52.15.181.225/GPT/Encrypt.html with API Key and code canyon purchase code.
